c18b7bb74534e9767d77627b2a00a710a54acf33
Guido8383 <guidorighi@gmail.com>
2025-05-12 13:20:42 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -27,3 +27,25 @@ def lingua(testo):
     print(f"La lingua rilevata è: {lingua}")
 
     return f"La lingua rilevata è: {lingua}"
+
+
+@app.get('/sentiment')
+def sentiment(frase):
+    from textblob import TextBlob
+
+    #frase = "I'm happy!"
+    blob = TextBlob(frase)
+
+    # Se sentiment > 0 allora -> positivo
+    # Se sentiment < 0 allora -> negativo
+    # Se sentiment = 0 allora -> neutro
+    sentiment = blob.sentiment.polarity
+    print(sentiment)
+    if sentiment > 0:
+        sentiment = "positivo"
+    elif sentiment < 0: 
+        sentiment = "negativo"
+    else:           
+        sentiment = "neutro"
+
+    return f"sentimento: {sentiment}"
